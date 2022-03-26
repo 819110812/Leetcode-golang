e3420d672de731d160c4c11e4360b6e923257f06
aqiu <Siqiu.Chen@dentsplysirona.com>
2022-03-26 09:39:30 +0800

    add description

--- 06zigzagconversion/zigzagconversion.go
+++ 06zigzagconversion/zigzagconversion.go
@@ -1,42 +1,75 @@
 package _6zigzagconversion
 
 // TODO: 需要复习
+//func convert(s string, numRows int) string {
+//	if numRows == 1 {
+//		return s
+//	}
+//
+//	// matrix 用来放置字符的二维数组
+//	// down 初始值为0，意思这个指针在最开始的第一层,用来控制向下的方向
+//	// up 初始值为 numRows-2，位于倒数第二行，numRows-1是最后一行的数组表达位置，则numRows-2为数组中的倒数第二行
+//	maxtrix, down, up := make([][]byte, numRows, numRows), 0, numRows-2
+//	for i := 0; i != len(s); {
+//		// down 二维数组顺序往下填
+//		// 未到下边界
+//		if down != numRows {
+//			maxtrix[down] = append(maxtrix[down], s[i])
+//			down++
+//			i++
+//		} else if up != 0 {
+//			// 已经到了下边界，开始向上挪
+//			// up不能超过上边界
+//			maxtrix[up] = append(maxtrix[up], s[i])
+//			up--
+//			i++
+//		} else {
+//			// 重置指针
+//			up = down - 2
+//			down = 0
+//		}
+//
+//	}
+//	// 收集最终的结果
+//	res := make([]byte, 0, len(s))
+//	for _, v := range maxtrix {
+//		for _, vv := range v {
+//			res = append(res, vv)
+//		}
+//	}
+//
+//	return string(res)
+//}
+
 func convert(s string, numRows int) string {
 	if numRows == 1 {
 		return s
 	}
 
-	// matrix 用来放置字符的二维数组
-	// down 初始值为0，意思这个指针在最开始的第一层,用来控制向下的方向
-	// up 初始值为 numRows-2，位于倒数第二行，numRows-1是最后一行的数组表达位置，则numRows-2为数组中的倒数第二行
-	maxtrix, down, up := make([][]byte, numRows, numRows), 0, numRows-2
-	for i := 0; i != len(s); {
-		// down 二维数组顺序往下填
-		// 未到下边界
-		if down != numRows {
-			maxtrix[down] = append(maxtrix[down], s[i])
+	down, up := 0, numRows-2
+	matrix := make([][]byte, numRows, numRows)
+
+	for i := 0; i < len(s); {
+		if down < numRows {
+			matrix[down] = append(matrix[down], s[i])
 			down++
 			i++
-		} else if up != 0 {
-			// 已经到了下边界，开始向上挪
-			// up不能超过上边界
-			maxtrix[up] = append(maxtrix[up], s[i])
+		} else if up > 0 {
+			matrix[up] = append(matrix[up], s[i])
 			up--
 			i++
 		} else {
-			// 重置指针
-			up = down - 2
 			down = 0
-		}
-
-	}
-	// 收集最终的结果
-	res := make([]byte, 0, len(s))
-	for _, v := range maxtrix {
-		for _, vv := range v {
-			res = append(res, vv)
+			up = numRows - 2
 		}
 	}
 
+	res := make([]byte, 0)
+	for _, row := range matrix {
+		for _, col := range row {
+			res = append(res, col)
+		}
+	}
 	return string(res)
+
 }
--- 09PalindromeNumber/PalindromeNumber.go
+++ 09PalindromeNumber/PalindromeNumber.go
@@ -1,21 +1,64 @@
 package PalindromeNumber
 
+//给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+//
+//回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+//
+//例如，121 是回文，而 123 不是。
+//
+//
+//示例 1：
+//
+//输入：x = 121
+//输出：true
+//示例2：
+//
+//输入：x = -121
+//输出：false
+//解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+//示例 3：
+//
+//输入：x = 10
+//输出：false
+//解释：从右向左读, 为 01 。因此它不是一个回文数
+//
+
+//func IsPalindrome(x int) bool {
+//	temp := x
+//	if x == 0 {
+//		return true
+//	}
+//	if x < 0 || (x%10 == 0) {
+//		return false
+//	}
+//	var reverse int
+//	for x >= 10 {
+//		reverse = x%10 + reverse*10
+//		x = x / 10
+//	}
+//	if x > 0 {
+//		reverse = x%10 + reverse*10
+//	}
+//	return reverse == temp
+//
+//}
+
 func IsPalindrome(x int) bool {
-	temp := x
 	if x == 0 {
 		return true
 	}
 	if x < 0 || (x%10 == 0) {
 		return false
 	}
-	var reverse int
+
+	temp := x
+	var reversed int
 	for x >= 10 {
-		reverse = x%10 + reverse*10
-		x = x / 10
+		reversed = reversed*10 + x%10
+		x /= 10
 	}
 	if x > 0 {
-		reverse = x%10 + reverse*10
+		reversed = reversed*10 + x%10
 	}
-	return reverse == temp
-
+	return temp == reversed
 }
